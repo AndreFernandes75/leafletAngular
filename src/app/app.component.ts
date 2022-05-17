e63f6b58448d92448e8861853f1d1ca4d759fc9e
AndreFernandes75 <84513676+AndreFernandes75@users.noreply.github.com>
2022-05-17 17:27:51 +0100

    Uncomplete
    error in editable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,149 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as L from 'leaflet';
+
+
+
+
+const ZERO_OPACITY = 0;
+const HALF_OPACITY = 0.5;
+const DRAWING_COMMIT = 'editable:drawing:commit';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'leafletAngular';
+  map!:L.Map;
+
+  
+  
+  ngOnInit(): void {
+    this.map = L.map('map', {editable:true} ).setView([38.72726949553772, -9.13994942204751], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  }
+
+  //ngAfterViewInit(): void {
+    //var map = L.map('map').setView([38.72726949553772, -9.13994942204751], 13);
+
+   
+
+    /*var marker = L.marker([38.72726949553772, -9.13994942204751]).addTo(map);
+
+    var polygon = L.polygon([
+      [38.7369109882447, -9.184238056304734],
+      [38.69619361222479, -9.214450457968574],
+      [38.72967999071096, -9.15024910443291]
+    ]).addTo(map);
+
+   var circle = L.circle([38.74012452996236, -9.213763812476214], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 500
+    }).addTo(map);
+
+    var popup = L.popup()
+      .setLatLng([38.72405537546273, -9.189387897497435])
+      .setContent("I am a standalone popup.")
+      .openOn(map);*/
+
+  
+
+/*
+    var buttonMarker = document.getElementById("buttonMarker");
+    var buttonCircle = document.getElementById("buttonCircle");
+    var coord = document.getElementById("coor");
+
+    buttonMarker?.addEventListener('click',function(){
+
+    map.on('click', <LeafletMouseEvent> (e:any) =>{
+      if(coord != null){
+      coord.textContent = e.latlng.lat, e.latlng.lng;}
+      var marker = L.marker([e.latlng.lat,e.latlng.lng]).addTo(map);
+     
+    });
+    
+    });
+
+    buttonCircle?.addEventListener('click',function(){
+
+     
+
+      map.on('click', <LeafletMouseEvent> (e:any) =>{
+        if(coord != null){
+          coord.textContent = e.latlng.lat, e.latlng.lng;}
+        var c = L.circle([e.latlng.lat,e.latlng.lng], {radius: 200}).addTo(map);
+        
+      });
+
+    
+      
+      });
+  
+
+    
+
+ 
+    
+    
+  }*/
+
+  onClickDraw(drawOption: string){
+    this.map.removeEventListener('click')
+    this.map.addEventListener(DRAWING_COMMIT,(event:any)=>{
+      this.map.on('click', <LeafletMouseEvent> (e:any) =>{
+        let geometryLayer = this.handleClickDrawOption(drawOption,e);
+        this.map.editTools.startPolygon(undefined);
+        // if(geometryLayer){
+        //   geometryLayer.addTo(this.map); 
+        // }
+      });
+      
+      });
+   console.log(drawOption)
+  }
+
+  handleClickDrawOption(action:string,event:any):L.Marker|L.Circle|L.Polygon|undefined{
+
+    //let marker = L.marker([e.latlng.lat,e.latlng.lng]).addTo(this.map);
+
+    switch (action) {
+      case "marker":
+        let marker:L.Marker = event.layer
+        return marker
+        break;
+
+        case "circle":
+          let circle:L.Circle = event.layer
+        return circle
+        break;
+    
+          
+          case "polygon":
+            let polygon:L.Polygon = event.layer
+            return polygon
+            break;
+      
+      default: 
+      return
+        break;
+    }
+
+  }
+
+  // drawPolygon(lineOptions: L.PolylineOptions): void {
+  //   this.map.addEventListener(DRAWING_COMMIT, (event) => {
+  //   const layer: L.Polygon = event.layer;
+  //   this.polygonArea = layer;
+  //   this.observeDrawingLayer(layer, event.type);
+  //   });
+  //   this.map.editTools.startPolygon(undefined, lineOptions);
+  //   }
 }
