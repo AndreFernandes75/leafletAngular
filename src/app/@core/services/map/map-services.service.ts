6966d983f0c40fa5dbb35d3dd9f84d20b47400f1
AndreFernandes75 <84513676+AndreFernandes75@users.noreply.github.com>
2022-06-09 16:00:29 +0100

    Adding comments in the project for the better understood of code

--- src/app/@core/services/map/map-services.service.ts
+++ src/app/@core/services/map/map-services.service.ts
@@ -22,9 +22,6 @@ const DRAWING_COMMIT = 'editable:drawing:commit';
 
 export class MapService {
 
-  private map!: L.Map;
-  
-
   namesOfBaseMaps = {
     'Google Maps': L.tileLayer(
       'https://mt.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
@@ -89,29 +86,13 @@ export class MapService {
     ),
   };
 
-  public lineOptions: L.PolylineOptions =
-    {
-      color: '#ff0000', lineJoin: 'round',
-    };
-
-
-
+  private map!: L.Map;
+  public drawOptions: any;
+  public coordinates: any;
   private polygonArea?: L.Polygon;
   private circleArea?: L.Circle;
-
   title = 'leafletAngular';
- 
   private markerArea?: L.Marker;
-
-  private markerIcon = L.icon({
-    iconUrl: 'assets/marker.svg',
-    iconSize: [38, 95], // size of the icon
-    shadowSize: [50, 64], // size of the shadow
-    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62], // the same for the shadow
-    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
-  });
-
   public pointMarker: any;
   public getPoint: boolean = false;
   public scale?: number;
@@ -123,15 +104,26 @@ export class MapService {
     }
   }
 
+  public lineOptions: L.PolylineOptions =
+    {
+      color: '#ff0000', lineJoin: 'round',
+    };
+
+  private markerIcon = L.icon({
+    iconUrl: 'assets/marker.svg',
+    iconSize: [38, 95], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62], // the same for the shadow
+    popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+  });
+
   public markerOptions: L.MarkerOptions = {
     icon: this.markerIcon,
   };
 
 
-  public drawOptions: any;
-  public coordinates: any;
-
-
+  //FUNCTION THAT INITIALIZE THE MAP
   public initializeMap(
     divId: string,
     center: [number, number],
@@ -168,18 +160,28 @@ export class MapService {
     });
   }
 
- 
-
-
+  //FUNCTION THAT OBSERVE THE LAYER AND THE EVENT.TYPE  
   observeDrawingLayer(
     layer: L.Polygon | L.Polyline | L.Marker,
     type: string
   ) {
   }
 
+  //FUNCTION THAT CLEAR EVERY DRAWS
+  public clearMap(): void {
+
+    if (this.polygonArea) {
+      this.map?.removeLayer(this.polygonArea);
+    }
+    if (this.markerArea) {
+      this.map?.removeLayer(this.markerArea)
+    }
+  }
+
 
 
   //FUNCTION THAT DETECTS WHICH BUTTON WAS CLICK AND EXECUTE A DETERMINATE TASK ACCORDING THE OPTION CHOOSEN
+  //IN THE MOMENT IS NOT BEEN USED
   handleClickDrawOption(action: any, event: any): L.Marker | L.Circle | L.Polygon | undefined {
 
 
@@ -210,25 +212,24 @@ export class MapService {
 
 
   drawPolygon(lineOptions: L.PolylineOptions): void {
+    //CLEAR THE PREVIOUS DRAWS
+    this.clearMap();
 
+    //ADD EVENT LISTENNER TO THE BUTTON AND ALLOW DRAW AND DRAG
     this.map.addEventListener(DRAWING_COMMIT, (event) => {
       const layer: L.Polygon = event.layer;
-      //layer.disableEdit();;
-
       this.polygonArea = layer;
       this.observeDrawingLayer(layer, event.type);
-    
 
-      
-
+      //FUNCTION THAT ALLOW TO KNOW THE COORDINATES AND UPDATE EVERY TIME WHEN IS DRAG
       this.polygonArea.on('editable:vertex:dragend', function (e) {
         let coord = layer.getBounds().getCenter();
         document.getElementById("coor")!.innerHTML = coord.toString()
-        
+
       });
 
-
     });
+    //LINE THAT STARTS THE FUNCTION TO DRAW A POLYGON
     this.map.editTools.startPolygon(undefined, lineOptions);
 
   }
@@ -236,15 +237,17 @@ export class MapService {
 
 
   pinMarker(markerOptions: L.MarkerOptions) {
-   
-    this.map?.addEventListener(DRAWING_COMMIT, (event) => {
+    //CLEAR THE PREVIOUS DRAWS
+    this.clearMap();
 
+    //ADD EVENT LISTENNER TO THE BUTTON AND ALLOW DRAW AND DRAG
+    this.map?.addEventListener(DRAWING_COMMIT, (event) => {
       const layer = event.layer;
-      // layer.disableEdit();
       this.markerArea = layer;
       this.observeDrawingLayer(layer, event.type);
       this.coordinates = layer._latlng
 
+      //FUNCTION THAT ALLOW TO KNOW THE COORDINATES AND UPDATE EVERY TIME WHEN IS DRAG
       if (this.markerArea != undefined) {
         this.markerArea.on('dragend', function (e) {
           let coord = layer._latlng;
@@ -252,46 +255,41 @@ export class MapService {
         });
       }
 
-
-
     });
+    //LINE THAT STARTS THE FUNCTION TO DRAW A MARKER
     this.map?.editTools.startMarker(undefined, markerOptions);
-
-
-
   }
 
 
 
 
   drawCircle(lineOptions: L.CircleMarkerOptions): void {
-    
+    //CLEAR THE PREVIOUS DRAWS
+    this.clearMap();
+
+    //ADD EVENT LISTENNER TO THE BUTTON AND ALLOW DRAW AND DRAG
     this.map.addEventListener(DRAWING_COMMIT, (event) => {
       const layer: L.Polygon = event.layer;
-      //layer.disableEdit();
       this.polygonArea = layer;
       this.observeDrawingLayer(layer, event.type);
 
-   
-
+      //FUNCTION THAT ALLOW TO KNOW THE COORDINATES AND UPDATE EVERY TIME WHEN IS DRAG
       this.polygonArea.on('editable:vertex:dragend', function (e) {
         let coord = layer.getBounds().getCenter();
         document.getElementById("coor")!.innerHTML = coord.toString()
-        
+
       });
 
     });
+    //LINE THAT STARTS THE FUNCTION TO DRAW A CIRCLE
     this.map.editTools.startCircle(undefined, lineOptions);
 
 
-    /*if (this.markerArea != undefined) {
-        this.markerArea.on('dragend', function (e) {
-          let coord = layer._latlng;
-          document.getElementById("coor")!.innerHTML = coord
-        });
-      }*/
+
 
   }
+
+
 
 
 
